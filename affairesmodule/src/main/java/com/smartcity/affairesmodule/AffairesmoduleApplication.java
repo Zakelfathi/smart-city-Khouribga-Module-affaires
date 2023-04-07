128dfb4845fa38bc37818dd1f87e98a0c31ee649
Achraf AISSY <105090484+AissyAchraf@users.noreply.github.com>
2023-04-07 14:26:22 +0000

    Initialisation of the Project

---
+++ affairesmodule/src/main/java/com/smartcity/affairesmodule/AffairesmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.smartcity.affairesmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffairesmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffairesmoduleApplication.class, args);
+	}
+
+}
---
+++ affairesmodule/src/test/java/com/smartcity/affairesmodule/AffairesmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcity.affairesmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffairesmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
