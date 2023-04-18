e53d128a6c27294983c386b37f5f82ae401e8740
Achraf AISSY <105090484+AissyAchraf@users.noreply.github.com>
2023-04-18 15:44:57 +0000

    upload centres affaires logos

--- affairesmodule/src/main/java/com/smartcity/affairesmodule/web/CentresAffairesController.java
+++ affairesmodule/src/main/java/com/smartcity/affairesmodule/web/CentresAffairesController.java
@@ -8,9 +8,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Controller
 public class CentresAffairesController {
@@ -45,8 +54,28 @@ public class CentresAffairesController {
     }
 
     @RequestMapping(value = "/admin/saveCentreAffaires", method = RequestMethod.POST)
-    public String saveCentreAffaire(Model model, centreAffaires centreAffaires) {
-        centreAffaireRepository.save(centreAffaires);
+    public String saveCentreAffaire(Model model, centreAffaires centreAffaires, @RequestParam("photo")MultipartFile multipartFile) throws IOException {
+        if(!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            centreAffaires.setLogo(fileName);
+            centreAffaires sevedCentreAffaires = centreAffaireRepository.save(centreAffaires);
+            String uploadDir = "./images/CentresAffaires/" + sevedCentreAffaires.getId();
+            Path uploadPath = Paths.get(uploadDir);
+            if (!Files.exists(uploadPath)) {
+                try {
+                    Files.createDirectories(uploadPath);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            try (InputStream inputStream = multipartFile.getInputStream()) {
+                Path filePath = uploadPath.resolve(fileName);
+                Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                throw new IOException("Could not save uploaded file : " + fileName);
+            }
+        }else
+            centreAffaireRepository.save(centreAffaires);
         return "redirect:/admin/centres-affaires";
     }
 
