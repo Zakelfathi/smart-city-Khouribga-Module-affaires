e1081535fe782b3641410694625c4ae08dd74ae4
Zakaria EL FATHI <70219190+Zakelfathi@users.noreply.github.com>
2023-04-20 06:52:32 +0000

    setup all app views(squelettes)

--- affairesmodule/src/main/java/com/smartcity/affairesmodule/web/appController.java
+++ affairesmodule/src/main/java/com/smartcity/affairesmodule/web/appController.java
@@ -9,6 +9,44 @@ public class appController {
 
     @GetMapping("/home")
     public String home(Model model) {
+
         return "app/home";
     }
+
+    @GetMapping("/historique")
+    public String historique(Model model) {
+
+        return "app/historique";
+    }
+    @GetMapping("/entreprises")
+    public String entreprises(Model model) {
+
+        return "app/entreprises";
+    }
+
+    @GetMapping("/affaires")
+    public String affaires(Model model) {
+        return "app/affaires";
+    }
+    @GetMapping("/gallerie")
+    public String gallerie(Model model) {
+        return "app/gallerie";
+    }
+    @GetMapping("/contact_us")
+    public String contactus(Model model) {
+        return "app/contactus";
+    }
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        return "app/profile";
+    }
+    @GetMapping("/edit_profile/id")
+    public String edit_profile(Model model) {
+        return "app/profile_edit";
+    }
+    @GetMapping("/loginn")
+    public String login(Model model) {
+        return "app/login";
+    }
+
 }
