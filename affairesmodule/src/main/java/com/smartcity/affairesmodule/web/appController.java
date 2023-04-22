aa637c9d1233fdddbf3d3668282d016fa3f69754
Zakaria EL FATHI <70219190+Zakelfathi@users.noreply.github.com>
2023-04-22 00:12:28 +0000

    setup entreprises view and stuffs

--- affairesmodule/src/main/java/com/smartcity/affairesmodule/web/appController.java
+++ affairesmodule/src/main/java/com/smartcity/affairesmodule/web/appController.java
@@ -1,15 +1,29 @@
 package com.smartcity.affairesmodule.web;
 
+import com.smartcity.affairesmodule.entities.entreprise;
+import com.smartcity.affairesmodule.entities.ville;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.smartcity.affairesmodule.repositories.VilleRepository;
+import com.smartcity.affairesmodule.repositories.EntrepriseRepository;
+
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class appController {
 
+    @Autowired
+    private VilleRepository VilleRepository;
+    @Autowired
+    private EntrepriseRepository EntrepriseRepository;
+
     @GetMapping("/home")
     public String home(Model model) {
-
         return "app/home";
     }
 
@@ -19,7 +33,14 @@ public class appController {
         return "app/historique";
     }
     @GetMapping("/entreprises")
-    public String entreprises(Model model) {
+    public String entreprises(Model model,
+                              @RequestParam(name="page", defaultValue="0") int page,
+                              @RequestParam(name="size", defaultValue="5") int size,
+                              @RequestParam(name="nom", defaultValue="") String nom) {
+
+        Optional<ville> ville = VilleRepository.findById(1L); // get the ville with id=1(khouribga)
+        List<entreprise> entreprises = EntrepriseRepository.findByVille(ville); // get the entreprises related to the ville
+        model.addAttribute("entreprises", entreprises);
 
         return "app/entreprises";
     }
